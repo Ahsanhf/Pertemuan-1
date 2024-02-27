81cc78b643ef6cab1e0d273c940b6fe098deb98a
ahsanhf <ahsanhf@gmail.com>
2024-02-27 19:16:19 +0700

    memasukan hider

--- Pertemuan 1/Pertemuan 1.cpp
+++ Pertemuan 1/Pertemuan 1.cpp
@@ -11,6 +11,9 @@ int main()
     //compute nLuas=3.14 * r * r
     //display 'Luasnya=' + nLuas
     float nJejari, nLuas; 
+    cout << "======Luas Lingkaran======" << endl;
+    cout << "Masukan Jejari = ";
+    cin >> nJejari;
  
 
 
